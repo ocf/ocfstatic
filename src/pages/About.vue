d9a85bf985756bba318866eea21a2a147ed1e772
bentref <bentref11@gmx.com>
2020-10-01 18:02:23 -0700

    Add very basic content to about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -5,9 +5,19 @@
         <h1 class="title">
           About Us
         </h1>
+        <p>
+          We're a student organization dedicated to providing free computing for
+          all UC Berkeley students, faculty, and staff. We run a super awesome
+          computer lab on southside, have a wicked set of servers for staff to
+          play on, and provide cool services to the Berkeley community.
+        </p>
         <h2 class="subtitle">
-          Hehe hoho
+          OCF in this Time of Corona
         </h2>
+        <p>
+          Although we can't meet in-person at our lab, we're still doing lots of
+          cool things!
+        </p>
       </div>
     </section>
     <section class="section has-text-centered">
