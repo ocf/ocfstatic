5240a2333f61f3df3e0d77fe83695bc06281a3ca
bentref <bentref11@gmx.com>
2020-10-11 16:23:34 -0700

    Add hero banners with images, beginning to put together layout of About page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -19,11 +19,10 @@
             </div>
             <div class="column is-half">
               <h2 class="subtitle">
-                OCF in this Time of Corona
+                Staff Meetings
               </h2>
               <p>
-                Although we can't meet in-person at our lab, we're still doing
-                lots of cool things! For example:
+                We meet every Monday at 8pm Pacific Time.
               </p>
               <ul>
                 <li>Online staff meetings</li>
@@ -34,6 +33,63 @@
         </div>
       </div>
     </section>
+    <section class="hero is-warning">
+      <div class="hero-body columns">
+        <div class="column is-quarter">
+          <img
+            src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fassets3.thrillist.com%2Fv1%2Fimage%2F1299823%2Fsize%2Ftl-horizontal_main%2F7-weird-stock-images-of-people-struggling-with-basic-cooking&f=1&nofb=1"
+          />
+        </div>
+        <div class="column is-three-quarters">
+          <div class="columns">
+            <div class="column is-third">
+              <!--<div class="content">-->
+              <h2 class="title is-2">Hello</h2>
+              <p>Lap aorwe apeg aslfkgj awklej wlrkgja sldfkgjawkaf d</p>
+              <!--</div>-->
+            </div>
+            <div class="column is-third">
+              <h2>Hello</h2>
+              <p>Lap aorwe apeg aslfkgj awklej wlrkgja sldfkgjawkaf d</p>
+            </div>
+            <div class="column is-third">
+              <h2>Hello</h2>
+              <p>Lap aorwe apeg aslfkgj awklej wlrkgja sldfkgjawkaf d</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="hero is-primary">
+      <div class="hero-body columns">
+        <div class="column is-three-quarters">
+          <div class="columns">
+            <div class="column is-third">
+              <!--<div class="content">-->
+              <h2 class="title is-2">Hello</h2>
+              <p>Lap aorwe apeg aslfkgj awklej wlrkgja sldfkgjawkaf d</p>
+              <!--</div>-->
+            </div>
+            <div class="column is-third">
+              <h2>Hello</h2>
+              <p>Lap aorwe apeg aslfkgj awklej wlrkgja sldfkgjawkaf d</p>
+            </div>
+            <div class="column is-third">
+              <h2>Hello</h2>
+              <p>Lap aorwe apeg aslfkgj awklej wlrkgja sldfkgjawkaf d</p>
+            </div>
+          </div>
+        </div>
+        <div class="column is-quarter">
+          <img
+            src="https://thumbs.dreamstime.com/z/person-money-computer-2932001.jpg"
+          />
+        </div>
+      </div>
+    </section>
+
+    <!--- The timeline -->
+
     <section class="section has-text-centered">
       <div class="container">
         <h1 class="title">
