4d3b1f90cde4e67445412aace9de61bbe84231d0
Bernard Zhao <bernard.zhao.us@gmail.com>
2020-11-09 18:09:40 -0800

    Updated syntax highlighting

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import "bulma-divider/dist/css/bulma-divider.min.css";
 import "@mdi/font/css/materialdesignicons.min.css";
 import axios from "axios";
 import initKeycloak from "~/auth.js";
-import "prismjs/themes/prism.css";
+import "~/assets/ocf-prism.css"; // Our custom css for styling code blocks
 
 export default async function(Vue, { isClient }) {
   // Set default layout as a global component
