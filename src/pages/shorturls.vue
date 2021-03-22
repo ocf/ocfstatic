15889cd9b072993f29915319c89f2df83836d70b
Justin Zhang <jyxzhang@berkeley.edu>
2021-03-22 17:25:38 -0400

    added table contents

--- src/pages/shorturls.vue
+++ src/pages/shorturls.vue
@@ -10,17 +10,9 @@
     </head>
     <body>
       <div class="m-3">
-        <h1>ocf.io short link translations</h1>
-        <h5><a href="https://www.ocf.berkeley.edu/docs/">Documentation</a></h5>
-        <hr />
+        <h1>Ocf.io Short Link Translations</h1>
         <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Short (ocf.io)</th>
-              <th scope="col">Long</th>
-            </tr>
-          </thead>
-          <tbody></tbody>
+          <ShortUrlTable />
         </table>
       </div>
     </body>
@@ -28,9 +20,14 @@
 </template>
 
 <script>
+import ShortUrlTable from "~/components/ShortUrlTable.vue";
+
 export default {
   metaInfo: {
     title: "Short Links"
+  },
+  components: {
+    ShortUrlTable
   }
 };
 </script>
