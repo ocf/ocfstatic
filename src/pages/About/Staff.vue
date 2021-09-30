a4a7cf5d6851e4fc1c58ae3f1cdc262e779f91de
Ronit Nath <69232394+RonitNath@users.noreply.github.com>
2021-09-29 20:45:31 -0700

    Change main people

--- src/pages/About/Staff.vue
+++ src/pages/About/Staff.vue
@@ -10,19 +10,25 @@
           Here are some of the volunteer staff who make the OCF go round!
         </p>
         <br />
-        <OfficerCardSet
+        
+        <OfficerCardSetanted="
           role-wanted="Staff"
           :cards="cards"
           :double-set="false"
         />
         <br />
+        
         <div class="is-divider"></div>
-
+        
         <OfficerCardSet
           role-wanted="Henlo"
           :cards="cards"
           :double-set="false"
         />
+        <br />anted="
+        
+        <div class="is-divider"></div>
+
       </div>
     </section>
   </Layout>
@@ -47,84 +53,45 @@ export default {
     this.cards = [
       {
         id: 1,
-        name: "Kevin Mo",
+        name: "Ronit",
         handle: "kmo",
         role: ["Staff"],
         face: "https://bulma.io/images/placeholders/1280x960.png",
         icon: "https://bulma.io/images/placeholders/96x96.png"
       },
       {
-        id: 2,
-        name: "Saurabh Narain ",
-        handle: "snarain",
+        id: 1,
+        name: "Ronit",
+        handle: "kmo",
+        role: ["Staff"],
+        face: "https://bulma.io/images/placeholders/1280x960.png",
+        icon: "https://bulma.io/images/placeholders/96x96.png"
+      },
+
+      {
+        id: 1,
+        name: "Ronit",
+        handle: "kmo",
         role: ["Staff"],
         face: "https://bulma.io/images/placeholders/1280x960.png",
         icon: "https://bulma.io/images/placeholders/96x96.png"
       },
       {
-        id: 3,
-        name: "Nikhil Jha  ",
-        handle: "njha",
+        id: 1,
+        name: "Ronit",
+        handle: "kmo",
         role: ["Staff"],
         face: "https://bulma.io/images/placeholders/1280x960.png",
         icon: "https://bulma.io/images/placeholders/96x96.png"
       },
-      {
-        id: 4,
-        name: "Frank Dai",
-        handle: "fydai",
-        role: ["Staff"],
-        face: "https://bulma.io/images/placeholders/1280x960.png",
-        icon: "https://bulma.io/images/placeholders/96x96.png"
-      },
-      {
-        id: 5,
-        name: "Ja Wattanawong",
-        handle: "jaw",
-        role: ["Staff"],
-        face: "https://bulma.io/images/placeholders/1280x960.png",
-        icon: "https://bulma.io/images/placeholders/96x96.png"
-      },
-      {
-        id: 6,
-        name: "Nikhil Jha",
-        handle: "njha",
+            {
+        id: 1,
+        name: "Ronit",
+        handle: "ronitnath",
         role: ["Henlo"],
         face: "https://bulma.io/images/placeholders/1280x960.png",
         icon: "https://bulma.io/images/placeholders/96x96.png"
       },
-      {
-        id: 7,
-        name: "Frank Dai",
-        handle: "fydai",
-        role: ["Henlo"],
-        face: "https://bulma.io/images/placeholders/1280x960.png",
-        icon: "https://bulma.io/images/placeholders/96x96.png"
-      },
-      {
-        id: 8,
-        name: "Ja Wattanawong",
-        handle: "jaw",
-        role: ["Henlo"],
-        face: "https://bulma.io/images/placeholders/1280x960.png",
-        icon: "https://bulma.io/images/placeholders/96x96.png"
-      },
-      {
-        id: 9,
-        name: "One person",
-        handle: "jsdx",
-        role: ["Henlo"],
-        face: "https://bulma.io/images/placeholders/1280x960.png",
-        icon: "https://bulma.io/images/placeholders/96x96.png"
-      },
-      {
-        id: 10,
-        name: "Another person",
-        handle: "sdfsdf",
-        role: ["Henlo"],
-        face: "https://bulma.io/images/placeholders/1280x960.png",
-        icon: "https://bulma.io/images/placeholders/96x96.png"
-      }
     ];
   }
 };
