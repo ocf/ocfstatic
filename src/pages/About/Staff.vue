947576b0ca5de5ac8037471c72053a6beba1a9f8
Huy Tran <63762580+huytt621@users.noreply.github.com>
2021-09-29 21:24:38 -0700

    Add myself to Staff

--- src/pages/About/Staff.vue
+++ src/pages/About/Staff.vue
@@ -148,6 +148,14 @@ export default {
         role: ["Online People"],
         face: "https://bulma.io/images/placeholders/1280x960.png",
         icon: "https://bulma.io/images/placeholders/96x96.png"
+      },
+      {
+        id: 889237,
+        name: "Huy Tran",
+        handle: "huytran",
+        role: ["SteelCow"],
+        face: "https://bulma.io/images/placeholders/1280x960.png",
+        icon: "https://bulma.io/images/placeholders/96x96.png"
       }
     ];
   }
