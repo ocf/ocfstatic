0cb9012aae3ce9d1343f23b50a4f3f1417f8e179
johnshale <59182078+johnshale@users.noreply.github.com>
2021-09-29 20:58:58 -0700

    fix syntax error

--- src/pages/About/Staff.vue
+++ src/pages/About/Staff.vue
@@ -132,6 +132,7 @@ export default {
         role: ["sudo rm -rf /"],
         face: "https://bulma.io/images/placeholders/1280x960.png",
         icon: "https://bulma.io/images/placeholders/96x96.png"
+      }
     ];
   }
 };
