b04279e6ea56fd127d011bb2881165af4ce899d1
bentref <bentref11@gmx.com>
2021-10-02 14:26:22 -0700

    add bentref to staff

--- src/pages/About/Staff.vue
+++ src/pages/About/Staff.vue
@@ -148,6 +148,14 @@ export default {
         role: ["Online People"],
         face: "https://bulma.io/images/placeholders/1280x960.png",
         icon: "https://bulma.io/images/placeholders/96x96.png"
+      },
+      {
+        id: 34927392710,
+        name: "Ben Trefry",
+        handle: "bentref",
+        role: ["Online People"],
+        face: "https://bulma.io/images/placeholders/1280x960.png",
+        icon: "https://bulma.io/images/placeholders/96x96.png"
       }
     ];
   }
