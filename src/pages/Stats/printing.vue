d51d421897afdf18a034b1085fd3a40098be38b3
bentref <bentref11@gmx.com>
2021-12-22 17:03:14 -0800

    Add printing stats page

--- src/pages/Stats/printing.vue
+++ src/pages/Stats/printing.vue
@@ -5,6 +5,7 @@
         <div class="tile">
           <div class="tile is-parent is-6">
             <div class="tile is-child box">
+              <h1 class="title is-3 has-text-centered">Printing Graphs</h1>
               <img
                 class=""
                 src="https://www.ocf.berkeley.edu/stats/printing/daily-job/graph"
@@ -15,19 +16,67 @@
               />
             </div>
           </div>
-          <div class="tile is-vertical">
+          <div class="tile">
             <div class="tile is-parent">
               <div class="tile is-child box">
                 <h3 class="title is-3 has-text-centered">Toner Usage</h3>
+                <table class="table">
+                  <tr>
+                    <th>Printer</th>
+                    <th>Toner Cartridges This Semester</th>
+                  </tr>
+                  <tr v-for="printer in tonerChanges">
+                    <td>{{ printer[0] }}</td>
+                    <td>{{ printer[1] }}</td>
+                  </tr>
+                </table>
               </div>
             </div>
             <div class="tile is-parent">
               <div class="tile is-child box">
-                <h3 class="title is-3 has-text-centered">
-                  Pages Printed Per Day
-                </h3>
+                <h3 class="title is-3 has-text-centered">Trees Killed</h3>
+                <table class="table">
+                  <tr>
+                    <th>Printer</th>
+                    <th>Trees killed over lifetime*</th>
+                  </tr>
+                  <tr v-for="dict in lifetimeData">
+                    <td>{{ dict.name }}</td>
+                    <td>
+                      {{
+                        Math.floor(dict.data[dict.data.length - 1][1] / 8333)
+                      }}
+                    </td>
+                  </tr>
+                </table>
+
+                <p>*Assuming each tree yields ~8333 pages.</p>
               </div>
             </div>
+          </div>
+        </div>
+        <div class="tile is-parent">
+          <div class="tile is-child box">
+            <h3 class="title is-3 has-text-centered">
+              Pages Printed Per Day
+            </h3>
+            <table class="table is-fullwidth">
+              <tr>
+                <th>Date</th>
+                <th>Total</th>
+                <th v-for="printer in activePrinters" :key="printer">
+                  {{ printer }}
+                </th>
+              </tr>
+
+              <tr v-for="day in Object.keys(pagesPerDay)">
+                <td>{{ day }}</td>
+                <td>{{ pagesPerDay[day]["total"] }}</td>
+                <td v-for="printer in activePrinters" :key="printer">
+                  {{ pagesPerDay[day][printer] }}
+                </td>
+              </tr>
+            </table>
           </div>
         </div>
       </div>
@@ -46,6 +95,43 @@ export default {
   },
   components: {
     Stats
+  },
+  data() {
+    return {
+      pagesPerDay: [],
+      activePrinters: [],
+      tonerUsage: [],
+      lifetimeData: [],
+      tonerChanges: []
+    };
+  },
+  methods: {
+    async setPagesPerDay() {
+      this.pagesPerDay = await this.$http
+        .get(this.$static.metadata.apiUrl + "printing/pages_per_day")
+        .then(response => response.data);
+    },
+    async setActivePrinters() {
+      this.activePrinters = await this.$http
+        .get(this.$static.metadata.apiUrl + "printing/active_printers")
+        .then(response => response.data);
+    },
+    async setLifetimeData() {
+      this.lifetimeData = await this.$http
+        .get(this.$static.metadata.apiUrl + "printing/lifetime_data")
+        .then(response => response.data);
+    },
+    async setTonerChanges() {
+      this.tonerChanges = await this.$http
+        .get(this.$static.metadata.apiUrl + "printing/toner_changes")
+        .then(response => response.data);
+    }
+  },
+  mounted() {
+    this.setPagesPerDay();
+    this.setActivePrinters();
+    this.setLifetimeData();
+    this.setTonerChanges();
   }
 };
 </script>
