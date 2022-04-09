0fffecb8e9614a11cdccd08ca3b366fa23e939e1
Ben Plate <bplate9583@gmail.com>
2022-04-09 12:23:09 -0700

    add gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `ocfstatic`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
