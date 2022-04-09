d32fe215fe5e30094e8c30436348c43f91d05acf
Ben Plate <bplate9583@gmail.com>
2022-04-09 16:08:49 -0700

    add hero section

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,54 @@
+import { Flex, Heading, Box, Button, Text } from "@chakra-ui/react"
+import React from "react"
+import OCFColors from "../definitions/OCFColors"
+import Logo from "./Logo"
+
+const HeroSection = () => {
+  return (
+    <Flex
+      w="100%"
+      h={600}
+      bg={OCFColors.primary}
+      p={20}
+      fontWeight="light"
+      color="white"
+    >
+      <Box flex={1} justifyContent="center">
+        <Box p={10}>
+          <Logo animated={true} />
+        </Box>
+      </Box>
+      <Flex flex={1} flexDirection="column" textAlign="center">
+        <Box flex={3}></Box>
+        <Heading flex={1}>Open Computing Facility </Heading>
+        <Text flex={1}>
+          An all-volunteer student organization dedicated to free computing for
+          all UC Berkeley students, faculty, and staff.{" "}
+        </Text>
+        <Flex flex={1} color="black">
+          <Button
+            h="100%"
+            flex={1}
+            marginLeft={5}
+            marginRight={5}
+            fontWeight="medium"
+          >
+            Create an Account
+          </Button>
+          <Button
+            h="100%"
+            flex={1}
+            marginLeft={5}
+            marginRight={5}
+            fontWeight="medium"
+          >
+            Host your Website
+          </Button>
+        </Flex>
+        <Box flex={3}></Box>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default HeroSection
---
+++ src/components/Logo.tsx
@@ -0,0 +1,59 @@
+import { styled } from "@chakra-ui/react"
+import React, { useEffect, useState } from "react"
+import OCFColors from "../definitions/OCFColors"
+
+import "./logo.css"
+
+export type LogoProps = {
+  height?: number
+  width?: number
+  animated?: boolean
+}
+
+const Logo = ({ height, width, animated }: LogoProps) => {
+  const [uuid, setUUID] = useState(0)
+
+  useEffect(() => {
+    setUUID(1)
+  })
+
+  return (
+    <svg
+      height={height}
+      width={width}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 630 285"
+      className="logo"
+    >
+      <path
+        fill={"url(#" + uuid + ")"}
+        className={animated ? "o" : ""}
+        stroke="black"
+        stroke-width="2"
+        d="M182.84,5h-100a7,7,0,0,0-6.34,5L64.4,55.17l-2.59,9.66L20.19,220.17l-2.59,9.66L5.45,275.17A3.65,3.65,0,0,0,9.16,280h140a6.88,6.88,0,0,0,6.29-4.83l12.15-45.34,2.59-9.66L211.81,64.83l2.59-9.66L226.5,10c.74-2.76-.9-5-3.66-5ZM120.19,220.17a6.89,6.89,0,0,1-6.3,4.83h-40a3.65,3.65,0,0,1-3.7-4.83L111.81,64.83a6.89,6.89,0,0,1,6.3-4.83h40a3.65,3.65,0,0,1,3.7,4.83Z"
+      />
+      <path
+        fill={"url(#" + uuid + ")"}
+        className={animated ? "c" : ""}
+        stroke="black"
+        stroke-width="2"
+        d="M408.11,60a6.88,6.88,0,0,0,6.29-4.83L426.55,9.83A3.65,3.65,0,0,0,422.84,5h-140a6.88,6.88,0,0,0-6.29,4.83L264.4,55.17l-2.59,9.66L220.19,220.17l-2.59,9.66-12.15,45.34a3.65,3.65,0,0,0,3.71,4.83h140a6.88,6.88,0,0,0,6.29-4.83l12.15-45.34a3.65,3.65,0,0,0-3.71-4.83h-90a3.65,3.65,0,0,1-3.7-4.83L311.81,64.83a6.89,6.89,0,0,1,6.3-4.83Z"
+      />
+      <path
+        fill={"url(#" + uuid + ")"}
+        className={animated ? "f" : ""}
+        stroke="black"
+        stroke-width="2"
+        d="M608.11,60a6.88,6.88,0,0,0,6.29-4.83L626.55,9.83A3.65,3.65,0,0,0,622.84,5h-140a6.88,6.88,0,0,0-6.29,4.83L464.4,55.17l-2.59,9.66-12.15,45.34-2.59,9.66-12.14,45.34-2.59,9.66L405.45,275.17a3.65,3.65,0,0,0,3.71,4.83h40a6.88,6.88,0,0,0,6.29-4.83l26.89-100.34a6.88,6.88,0,0,1,6.29-4.83h40a6.91,6.91,0,0,0,6.3-4.83l12.14-45.34a3.64,3.64,0,0,0-3.7-4.83h-40a3.65,3.65,0,0,1-3.71-4.83l12.15-45.34a6.89,6.89,0,0,1,6.3-4.83Z"
+      />
+      <defs>
+        <linearGradient id={uuid.toString()} gradientTransform="rotate(315)">
+          <stop offset="0%" stop-color="#000000" />
+          <stop offset="74%" stop-color="#414141" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default Logo
