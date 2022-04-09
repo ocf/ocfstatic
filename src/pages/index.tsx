9da54e5c9995187c1169aec5f1f4358016891135
Ben Plate <bplate9583@gmail.com>
2022-04-09 13:06:24 -0700

    add ocf logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Flex, Image, Link, Text } from "@chakra-ui/react"
 import React from "react"
+
+import logo from "../images/logo.svg";
+import penguin from "../images/penguin.svg";
 
 const IndexPage = () => {
   return (
-    <Box bg='tomato' w='100%' p={8} color='white'>
-      This is the Box
+    <Box bg='tomato' w='100%' h={100} paddingLeft={8} paddingRight={8} color='white'>
+      <a href="/" style={{float: "left", height: "50%", position: "relative", top: "25%"}}>
+        <Flex h="100%">
+            <Image src={penguin} h="100%" />
+            <Image src={logo} h="80%" position="relative" top="10%" />
+        </Flex>
+      </a>
     </Box>
   )
 }
