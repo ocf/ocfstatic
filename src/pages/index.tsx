cac864c453c4b1f4a4c180f1cd31f72c16fb641c
Kian <kian@ocf.berkeley.edu>
2022-04-16 19:59:48 -0700

    Add staff in lab section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
 import HeroSection from "~/components/HeroSection"
 import Navbar from "~/components/Navbar"
 import Footer from "~/components/Footer"
+import { useApiRoute } from "~/utils/api"
 
 const IndexPage = () => {
+  const { data: staffInLab } = useApiRoute("/lab/staff")
   return (
     <>
       <Navbar />
       <HeroSection />
+      <div>
+        {staffInLab && (
+          <>
+            <h2>Staff in Lab:</h2>
+            <ul>
+              {staffInLab.staff_in_lab.map((s) => (
+                <li key={s.host}>
+                  {s.user} on {s.host} since {s.start}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
       <Footer />
     </>
   )
