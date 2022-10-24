0401f594c209c053998d223223f079ec49aaec29
Ben Plate <bplate9583@gmail.com>
2022-10-23 17:21:05 -0700

    add account skeleton

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,10 +1,11 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Input, Text } from "@chakra-ui/react"
 import { useKeycloak } from "@react-keycloak/web"
 import { navigate } from "gatsby"
 import Footer from "~/components/Footer"
 import Layout from "~/components/Layout"
 import Navbar from "~/components/Navbar"
 import Sidebar from "~/components/Sidebar"
+import { OCFKeycloakToken } from "~/utils/keycloak"
 
 const AccountDashboardPage = () => {
   const { initialized, keycloak } = useKeycloak()
@@ -14,11 +15,49 @@ const AccountDashboardPage = () => {
     return null
   }
 
+  const profile = keycloak.tokenParsed as OCFKeycloakToken
+
   return (
     <Layout>
       <Navbar />
       <Box>
         <Sidebar />
+        <Box>
+          <Box h="20" />
+          <Text color="gray.500" fontSize="1.5rem" fontWeight="medium">
+            ACCOUNT
+          </Text>
+          <br />
+          <Box>
+            <Text fontWeight="medium">Name</Text>
+            <Input value={profile.given_name} maxWidth="500" disabled={true} />
+          </Box>
+          <br />
+          <Box>
+            <Text fontWeight="medium">OCF Username</Text>
+            <Input
+              value={profile.preferred_username}
+              maxWidth="500"
+              disabled={true}
+            />
+          </Box>
+          <br />
+          <Box>
+            <Text fontWeight="medium">Account Type</Text>
+            <Input value="Personal Account" maxWidth="500" disabled={true} />
+          </Box>
+          <br />
+          <Box>
+            <Text fontWeight="medium">Email</Text>
+            <Input value={profile.email} maxWidth="500" />
+          </Box>
+          <br />
+          <Box>
+            <Text fontWeight="medium">Password</Text>
+            <Input type="password" maxWidth="500" />
+          </Box>
+          <Box h="16" />
+        </Box>
       </Box>
       <Footer />
     </Layout>
