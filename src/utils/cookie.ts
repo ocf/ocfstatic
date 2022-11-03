f3096d961d9a7e9882e1af18c2fd455fd14745fa
Kian <kian@ocf.berkeley.edu>
2022-11-03 11:09:19 -0700

    Fix build errors

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,12 +1,11 @@
 export const getCookie = (key: string): string => {
-  if (typeof document !== undefined) {
-    return document.cookie.split("; ").reduce((total, currentCookie) => {
-      const item = currentCookie.split("=")
-      const storedKey = item[0]
-      const storedValue = item[1]
+  if (typeof document === "undefined") return ""
 
-      return key === storedKey ? decodeURIComponent(storedValue) : total
-    }, "")
-  }
-  return ""
+  return document?.cookie.split("; ").reduce((total, currentCookie) => {
+    const item = currentCookie.split("=")
+    const storedKey = item[0]
+    const storedValue = item[1]
+
+    return key === storedKey ? decodeURIComponent(storedValue) : total
+  }, "")
 }
